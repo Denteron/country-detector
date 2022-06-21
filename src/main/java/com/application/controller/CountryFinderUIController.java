230b6131cd046b01d8bccf13dd58bb42a52e5e6b
Denteron <redisbad11@gmail.com>
2022-06-21 20:14:33 +0300

    fix import

--- src/main/java/com/application/controller/CountryFinderUIController.java
+++ src/main/java/com/application/controller/CountryFinderUIController.java
@@ -1,14 +1,15 @@
 package com.application.controller;
 
 import com.application.service.IdentifyCountryService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/ui")
 public class CountryFinderUIController {
 
@@ -25,7 +26,6 @@ public class CountryFinderUIController {
     public String startPage() {
         return "index";
     }
-
 
 
 }
